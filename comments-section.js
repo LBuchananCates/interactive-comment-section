e886d3960cbec16d416875f8f7a8922457788ee4
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2022-12-30 14:56:54 -0500

    loops avatar, username, pot date, post in commentDiv

--- comments-section.js
+++ comments-section.js
@@ -1,64 +1,38 @@
 fetch("./data.json")
   .then((response) => response.json())
   .then((json) => {
-    // Create new comment & place inside comment-container
-    const commentDiv = document.createElement("div");
-
-    // create parag. tag & place inside comment-container
-    const commentParagraph1 = document.createElement("p");
-    const newComment1 = document.createTextNode(json.comments[0].content);
-    commentParagraph1.appendChild(newComment1);
-
-    // create second parag. tag & place inside comment-container
-    const commentParagraph2 = document.createElement("p");
-    const newComment2 = document.createTextNode(json.comments[1].content);
-    commentParagraph2.appendChild(newComment2);
-
-    // create vote buttons & place inside comment-container
-    const upvoteButton = document.createElement("button");
-    const plusSign = document.createTextNode("+");
-    upvoteButton.appendChild(plusSign);
-
-    // const totalVotes = document.createElement("span");
-    // const numVotes = document.createTextNode("12");
-    // totalVotes.appendChild(numVotes); THIS MIGHT BE COMPLETELY WRONG
-
-    const downvoteButton = document.createElement("button");
-    const minusSign = document.createTextNode("-");
-    downvoteButton.appendChild(minusSign);
-
-    //create reply button & place inside comment-container
-    const replyButton = document.createElement("button");
-    const reply = document.createTextNode("Reply");
-    replyButton.appendChild(reply);
-
-    // Put comment in container
-    const commentContainer = document.querySelector(".comment-container");
-    commentContainer.append(commentDiv);
-    commentContainer.append(commentParagraph1);
-    commentContainer.append(commentParagraph2);
-    commentContainer.append(upvoteButton);
-    commentContainer.append(downvoteButton);
-    commentContainer.append(replyButton);
-
     const comments = json.comments;
 
-    // // loop over array of comments
     for (const comment of comments) {
-      // console.log(comment.createdAt);
-      // create paragraph element
-      const createdAt = document.createElement("p");
-      // put createdAt inside paragraph
-      const postDate = document.createTextNode(comment.createdAt);
+      // create comment div
+      const commentDiv = document.createElement("div");
+      document.body.append(commentDiv);
+      // add avatar
+      const userAvatar = document.createElement("img");
+      userAvatar.src = comment.user.image.png;
+      commentDiv.append(userAvatar);
 
-      // put string into para
-      createdAt.appendChild(postDate);
-      console.log(createdAt);
-      // append to ctainer
-      commentContainer.append(createdAt);
+      // add username
+      const userName = document.createElement("span");
+      userName.textContent = comment.user.username;
+      commentDiv.append(userName);
+
+      // add post date
+      const postDate = document.createElement("p");
+      postDate.textContent = comment.createdAt;
+      commentDiv.append(postDate);
+
+      // add comment
+      const post = document.createElement("p");
+      post.textContent = comment.content;
+      commentDiv.append(post);
+
+      // add upvote and downvote
+
+      // add arrow icon
+
+      // add reply button
+
+      // put everything inside comment div
     }
-
-    // TODO: Style comment and buttons in them
-
-    // provide starting "score" for up and downvotes
   });
